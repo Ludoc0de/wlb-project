84aaa071ddc8678ec1606e0216d4e557fbf4bfba
ludoc0de <champi.ludo@tutanota.com>
2023-11-02 22:25:57 -0300

    create new project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">WLB</a>
+      </div>
+      <div className="flex-none">
+        <a className="btn btn-ghost normal-case text-xl">Contact</a>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,21 @@
+export default function Main() {
+  return (
+    <div
+      className="hero py-80"
+      style={
+        {
+          // backgroundImage: "url(/wlb.jpeg)",
+        }
+      }
+    >
+      <div className=""></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold text-yellow-400">W L B</h1>
+          <p className="mb-10 text-xl">Women luxury beauty</p>
+          <button className="btn btn-warning py-4">Accompte</button>
+        </div>
+      </div>
+    </div>
+  );
+}
