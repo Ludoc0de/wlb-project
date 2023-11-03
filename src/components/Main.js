f3cb081d6354a8f34688fded71d9949d654945c0
ludoc0de <champi.ludo@tutanota.com>
2023-11-02 22:49:13 -0300

    add fonts

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,8 +11,8 @@ export default function Main() {
       <div className=""></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold text-yellow-400">W L B</h1>
-          <p className="mb-10 text-xl">Women luxury beauty</p>
+          <h1 className="mb-5 text-6xl font-bold text-yellow-400 ">WLB</h1>
+          <p className="mb-10 text-3xl font-display">Women luxury beauty</p>
           <button className="btn btn-warning py-4">Accompte</button>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      display: ["Satisfy"],
+    },
   },
   plugins: [require("daisyui")],
   // daisyUI config (optional)
