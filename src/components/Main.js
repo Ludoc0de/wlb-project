92fbd048cf75726b7aa403f27edeb7ad5a1d2754
ludoc0de <champi.ludo@tutanota.com>
2023-11-05 18:43:03 -0300

    add gold color and update position

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,16 @@
 export default function Main() {
   return (
     <div
-      className="hero py-80 md:py-40"
+      className="hero flex justify-start py-80 md:py-40 ml-auto pl-4"
       style={
         {
           // backgroundImage: "url(/wlb.jpeg)",
         }
       }
     >
-      <div className=""></div>
-      <div className="hero-content text-center text-neutral-content">
+      <div className="hero-content text-left text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-6xl font-bold text-yellow-400 font-raleway">
-            WLB
-          </h1>
+          <h1 className="mb-5 text-6xl font-bold gold-wlb font-raleway">WLB</h1>
           <p className="mb-10 text-3xl font-satisfy">Women luxury beauty</p>
           <button className="btn btn-outline btn-warning py-4 px-8 font-satisfy">
             Accompte
