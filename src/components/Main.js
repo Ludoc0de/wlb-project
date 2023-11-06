a6a877da66c5f53ff46fe8ada93284bbabd6525f
ludoc0de <champi.ludo@tutanota.com>
2023-11-05 21:57:28 -0300

    up

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,42 @@
-export default function Main() {
-  return (
+import React, { useState, useEffect } from "react";
+import Product from "./Product";
+
+const Message = ({ message }) => (
+  <section>
     <div
       className="hero min-h-screen flex py-80 md:py-40 pl-8"
       style={{
-        backgroundImage: "url(/wlb131.jpeg)",
+        backgroundImage: "url(/wlb121.jpeg)",
       }}
     >
       <div className="hero-content text-left text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-6xl font-bold gold-wlb font-raleway">WLB</h1>
-          <p className="mb-10 text-3xl font-satisfy">Women luxury beauty</p>
-          <button className="btn btn-outline btn-warning py-4 px-8 font-satisfy">
-            Accompte
-          </button>
+          <p className="mb-10 text-3xl font-satisfy">{message}</p>
+          <li className="gold-wlb list-none">
+            <a href="/">Accueil</a>
+          </li>
         </div>
       </div>
     </div>
-  );
+  </section>
+);
+
+export default function Main() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+
+    if (query.get("success")) {
+      setMessage("Nous vous remercions! Votre réservation est confirmé");
+    }
+
+    if (query.get("canceled")) {
+      setMessage("Accompte annulée - revenez lorsque vous serez prêt.");
+    }
+  }, []);
+
+  return message ? <Message message={message} /> : <Product />;
 }
