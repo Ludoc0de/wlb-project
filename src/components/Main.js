63a54f0a6b0566341eead86a10973cc78c308d4f
ludoc0de <champi.ludo@tutanota.com>
2023-11-05 22:00:21 -0300

    remove div

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,21 +2,19 @@ import React, { useState, useEffect } from "react";
 import Product from "./Product";
 
 const Message = ({ message }) => (
-  <section>
-    <div
-      className="hero min-h-screen flex py-80 md:py-40 pl-8"
-      style={{
-        backgroundImage: "url(/wlb121.jpeg)",
-      }}
-    >
-      <div className="hero-content text-left text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-6xl font-bold gold-wlb font-raleway">WLB</h1>
-          <p className="mb-10 text-3xl font-satisfy">{message}</p>
-          <li className="gold-wlb list-none">
-            <a href="/">Accueil</a>
-          </li>
-        </div>
+  <section
+    className="hero min-h-screen flex py-80 md:py-40 pl-8"
+    style={{
+      backgroundImage: "url(/wlb121.jpeg)",
+    }}
+  >
+    <div className="hero-content text-left text-neutral-content">
+      <div className="max-w-md">
+        <h1 className="mb-5 text-6xl font-bold gold-wlb font-raleway">WLB</h1>
+        <p className="mb-10 text-3xl font-satisfy">{message}</p>
+        <li className="gold-wlb list-none">
+          <a href="/">Accueil</a>
+        </li>
       </div>
     </div>
   </section>
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,30 +2,26 @@ import { useState } from "react";
 
 export default function Product() {
   return (
-    <section>
-      <div
-        className="hero min-h-screen flex py-80 md:py-40 pl-4"
-        style={{
-          backgroundImage: "url(/wlb131.jpeg)",
-        }}
-      >
-        <div className="hero-content text-left text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-6xl font-bold gold-wlb font-raleway">
-              WLB
-            </h1>
-            <p className="mb-10 text-3xl font-satisfy font-bold">
-              Women luxury beauty
-            </p>
-            <form action="/subscription/create-checkout-session" method="POST">
-              <button
-                type="submit"
-                className="btn btn-outline btn-warning py-4 px-8 font-satisfy"
-              >
-                Accompte
-              </button>
-            </form>
-          </div>
+    <section
+      className="hero min-h-screen flex py-80 md:py-40 pl-4"
+      style={{
+        backgroundImage: "url(/wlb131.jpeg)",
+      }}
+    >
+      <div className="hero-content text-left text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-6xl font-bold gold-wlb font-raleway">WLB</h1>
+          <p className="mb-10 text-3xl font-satisfy font-bold">
+            Women luxury beauty
+          </p>
+          <form action="/subscription/create-checkout-session" method="POST">
+            <button
+              type="submit"
+              className="btn btn-outline btn-warning py-4 px-8 font-satisfy"
+            >
+              Accompte
+            </button>
+          </form>
         </div>
       </div>
     </section>
